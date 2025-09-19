4d4335d4c228d5a1c1b27ba2dfc1bf884f7673c5
sjcfkevin <sjcfkevin@localhost>
2025-09-18 17:17:28 -0700

    kevin nguyen

--- src/guiStudent/ControllerStudentHome.java
+++ src/guiStudent/ControllerStudentHome.java
@@ -11,7 +11,7 @@ public class ControllerStudentHome {
 	the Model is often just a stub, or will be a singleton instantiated object.
 	
 	 */
-
+//kevin
 	
  	/**********
 	 * <p> Method: performLogout() </p>
